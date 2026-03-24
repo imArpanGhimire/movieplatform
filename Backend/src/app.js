becbd41bc94f3933c64ddb1509dc85cf552fa53c
imArpanGhimire <arpancalma@gmail.com>
2026-03-25 00:17:33 +0545

    feat: add CORS support and Axios for API requests; update package dependencies

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -3,11 +3,19 @@ const app = express()
 const authroutes = require("./routes/auth.routes")
 const movieroutes = require("./routes/movie.routes")
 const cookieParser = require("cookie-parser")
+const cors = require("cors")
+
+app.use(
+    cors({
+        origin: "http://localhost:5173",
+        credentials: true
+    })
+)
+
 app.use(express.json())
 app.use(cookieParser())
 
 app.use("/api/auth", authroutes)
 app.use("/api/movie", movieroutes)
-
 
 module.exports = app
---
+++ Frontend/src/api/axios.js
@@ -0,0 +1,9 @@
+// Frontend/src/api/axios.js
+import axios from "axios"
+
+const api = axios.create({
+    baseURL: "http://localhost:3000/api",
+    withCredentials: true,
+})
+
+export default api
