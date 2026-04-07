61a28751f834e32cb019765a8e465204961a6f3b
imArpanGhimire <arpancalma@gmail.com>
2026-04-07 22:07:52 +0545

    CHANGED FONT ALL ACROSS SITE

--- Frontend/src/components/MovieCard.jsx
+++ Frontend/src/components/MovieCard.jsx
@@ -6,7 +6,7 @@ const MovieCard = ({ movie }) => {
       {/* Poster */}
       <div className="relative h-80 bg-linear-to-br from-teal-500 to-zinc-800 flex items-center justify-center overflow-hidden transition-all duration-500 group-hover:scale-105">
         {/* Big letter */}
-        <p className="relative z-10 text-5xl font-bold text-slate-300 capitalize transition-all duration-500 group-hover:scale-105 group-hover:opacity-5 group-hover:blur-sm">
+        <p className="relative z-10 text-6xl font-bold text-slate-300 capitalize transition-all duration-500 group-hover:scale-105 group-hover:opacity-5 group-hover:blur-sm">
           {movie.title?.charAt(0)}
         </p>
 
@@ -15,7 +15,7 @@ const MovieCard = ({ movie }) => {
 
         {/* Centered content */}
         <div className="absolute inset-0 z-20 flex flex-col items-center justify-center text-center px-4 opacity-0 translate-y-4 transition-all duration-500 group-hover:opacity-100 group-hover:translate-y-0">
-          <h3 className="max-w-[85%] text-xl font-bold text-slate-100 truncate">
+          <h3 className="max-w-[85%] text-2xl font-semibold text-slate-300 truncate">
             {movie.title}
           </h3>
 
