25d7c692d541644ecb33d98b5b0644bf364d9ff9
imArpanGhimire <arpancalma@gmail.com>
2026-04-20 00:01:50 +0545

    refactor togglelike function to improve like handling logic

--- Backend/src/controllers/like.controller.js
+++ Backend/src/controllers/like.controller.js
@@ -2,9 +2,44 @@ const likemodel = require("../models/likeModel")
 
 const togglelike = async (req, res) => {
     try {
+        const { reviewId } = req.body
+        const userId = req.user.id
 
-    }
-    catch (e) {
+        // check if already liked
+        const existing = await likemodel.findOne({
+            review: reviewId,
+            user: userId
+        })
 
+        // if already liked → remove
+        if (existing) {
+            await likemodel.findByIdAndDelete(existing._id)
+
+            const count = await likemodel.countDocuments({ review: reviewId })
+
+            return res.json({
+                liked: false,
+                likesCount: count
+            })
+        }
+
+        // if not liked → add
+        await likemodel.create({
+            review: reviewId,
+            user: userId
+        })
+
+        const count = await likemodel.countDocuments({ review: reviewId })
+
+        res.json({
+            liked: true,
+            likesCount: count
+        })
+
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({ message: "error in like" })
     }
 }
+
+module.exports = { togglelike }
