eff26e792bbb04fd8ba0f76a49a29a81e9c708f5
imArpanGhimire <arpancalma@gmail.com>
2026-04-20 00:42:56 +0545

    refactor liketoggle function to improve response handling and add error response

--- Backend/src/controllers/like.controller.js
+++ Backend/src/controllers/like.controller.js
@@ -2,7 +2,6 @@ const likemodel = require("../models/like.model")
 
 const liketoggle = async (req, res) => {
     try {
-
         const { reviewid } = req.body
         const userid = req.user.id
 
@@ -10,12 +9,26 @@ const liketoggle = async (req, res) => {
             review: reviewid,
             user: userid
         })
+
         console.log(existing)
+
+        // Add this response
+        if (existing) {
+            await likemodel.deleteOne({ _id: existing._id })
+            return res.json({ message: "Like removed" })
+        } else {
+            const newLike = await likemodel.create({
+                review: reviewid,
+                user: userid
+            })
+            return res.json({ message: "Like added", like: newLike })
+        }
     }
     catch (e) {
         console.log(e)
+        // Add error response
+        res.status(500).json({ error: "Failed to toggle like" })
     }
 }
-
 
 module.exports = { liketoggle }
