8da0c6585d940f3dbcac8acebfc6c4cd624ace23
imArpanGhimire <arpancalma@gmail.com>
2026-04-22 23:57:13 +0545

    fix: add missing line  in liketoggle function

--- Backend/src/controllers/like.controller.js
+++ Backend/src/controllers/like.controller.js
@@ -2,8 +2,8 @@ const likemodel = require("../models/like.model")
 
 const liketoggle = async (req, res) => {
     try {
-        const { reviewid } = req.body
-        const userid = req.user.id
+        const { reviewid } = req.body;
+        const userid = req.user.id;
 
         const existing = await likemodel.findOne({
             review: reviewid,
