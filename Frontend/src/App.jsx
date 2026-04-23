5f52a490c86168bf0dbb11ee20a4de2f9b7963e7
imArpanGhimire <arpancalma@gmail.com>
2026-04-24 01:33:07 +0545

    feat: refactor App component to use AppContent for better structure and navbar visibility control

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -4,6 +4,7 @@ import {
   Routes,
   Route,
   Navigate,
+  useLocation,
 } from "react-router-dom";
 import { Toaster } from "sileo";
 
@@ -16,48 +17,61 @@ import ProtectedRoute from "./protection/ProtectedRoute";
 import MovieDetailPage from "./components/MovieDetailPage";
 import Navbar from "./components/Navbar";
 
+function AppContent() {
+  const location = useLocation();
+
+  const hideNavbar =
+    location.pathname === "/login" || location.pathname === "/register";
+
+  return (
+    <>
+      <Toaster
+        options={{
+          fill: "oklch(37.3% 0.034 259.733)",
+          styles: {
+            description:
+              "text-white/75! text-center flex items-center justify-center flex-col w-full",
+            button: "px-3 py-1 w-[50%]",
+          },
+        }}
+        position="top-center"
+      />
+
+      {!hideNavbar && <Navbar />}
+
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+
+        <Route
+          path="/movies"
+          element={
+            <ProtectedRoute>
+              <MovieListingPage />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/movie/:id"
+          element={
+            <ProtectedRoute>
+              <MovieDetailPage />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route path="/" element={<Navigate to="/movies" />} />
+      </Routes>
+    </>
+  );
+}
+
 const App = () => {
   return (
     <ThemeProvider>
       <Router>
-        <Toaster
-          options={{
-            fill: "oklch(37.3% 0.034 259.733)",
-            styles: {
-              description:
-                "text-white/75! text-center flex items-center justify-center flex-col w-full",
-              button: "px-3 py-1 w-[50%]",
-            },
-          }}
-          position="top-center"
-        />
-
-        <Navbar />
-
-        <Routes>
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/register" element={<RegisterPage />} />
-
-          <Route
-            path="/movies"
-            element={
-              <ProtectedRoute>
-                <MovieListingPage />
-              </ProtectedRoute>
-            }
-          />
-
-          <Route
-            path="/movie/:id"
-            element={
-              <ProtectedRoute>
-                <MovieDetailPage />
-              </ProtectedRoute>
-            }
-          />
-
-          <Route path="/" element={<Navigate to="/movies" />} />
-        </Routes>
+        <AppContent />
       </Router>
     </ThemeProvider>
   );
