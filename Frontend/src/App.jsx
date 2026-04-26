0be5eb9c60db18141af528d99a23d924c9624207
imArpanGhimire <arpancalma@gmail.com>
2026-04-27 01:46:13 +0545

    refactor: reorganize imports and streamline Toaster component usage in App

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -8,30 +8,28 @@ import {
 import { Toaster } from "sileo";
 
 import { ThemeProvider } from "./context/ThemeContext";
+import Navbar from "./components/Navbar";
+
 import LoginPage from "./components/LoginPage";
 import RegisterPage from "./components/RegisterPage";
 import MovieListingPage from "./components/MovieListingPage";
-import ProtectedRoute from "./protection/ProtectedRoute";
 import MovieDetailPage from "./components/MovieDetailPage";
+import ProtectedRoute from "./protection/ProtectedRoute";
 
 const App = () => {
   return (
     <ThemeProvider>
       <Router>
-        <Toaster
-          options={{
-            fill: "oklch(37.3% 0.034 259.733)",
-            styles: {
-              description:
-                "text-white/75! text-center flex items-center justify-center flex-col w-full",
-              button: "px-3 py-1 w-[50%]",
-            },
-          }}
-          position="top-center"
-        />
+        <Navbar />
+
+        <Toaster position="top-center" />
+
         <Routes>
+          <Route path="/" element={<Navigate to="/login" />} />
+
           <Route path="/login" element={<LoginPage />} />
           <Route path="/register" element={<RegisterPage />} />
+
           <Route
             path="/movies"
             element={
@@ -40,6 +38,7 @@ const App = () => {
               </ProtectedRoute>
             }
           />
+
           <Route
             path="/movie/tmdb/:tmdbId"
             element={
@@ -48,7 +47,6 @@ const App = () => {
               </ProtectedRoute>
             }
           />
-          <Route path="/" element={<Navigate to="/movies" />} />
         </Routes>
       </Router>
     </ThemeProvider>
