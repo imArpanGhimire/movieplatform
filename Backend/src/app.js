fb3daf78a62c12f9820ec49aec91a6ea2a03e9f5
imArpanGhimire <arpancalma@gmail.com>
2026-04-29 23:05:38 +0545

    refactor: reorganize imports and middleware setup in app.js

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,21 +1,31 @@
-const express = require("express")
-const app = express()
-const authroutes = require("./routes/auth.routes")
-const movieroutes = require("./routes/movie.routes")
-const likesroutes = require("./routes/likes.routes")
-const replyRoutes = require("./routes/reply.routes")
-const tmdbRoutes = require("./routes/tmdb.routes")   // add this
-const cookieParser = require("cookie-parser")
-const cors = require("cors")
+const express = require("express");
+const app = express();
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }))
-app.use(express.json())
-app.use(cookieParser())
+const authroutes = require("./routes/auth.routes");
+const movieroutes = require("./routes/movie.routes");
+const likesroutes = require("./routes/likes.routes");
+const replyRoutes = require("./routes/reply.routes");
+const tmdbRoutes = require("./routes/tmdb.routes");
+const savedRoutes = require("./routes/saved.routes");
 
-app.use("/api/auth", authroutes)
-app.use("/api/movie", movieroutes)
-app.use("/api/toggle", likesroutes)
-app.use("/api/reply", replyRoutes)
-app.use("/api/tmdb", tmdbRoutes)   // add this
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
 
-module.exports = app
+app.use(
+    cors({
+        origin: "http://localhost:5173",
+        credentials: true,
+    })
+);
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.use("/api/auth", authroutes);
+app.use("/api/movie", movieroutes);
+app.use("/api/toggle", likesroutes);
+app.use("/api/reply", replyRoutes);
+app.use("/api/tmdb", tmdbRoutes);
+app.use("/api/saved", savedRoutes);
+
+module.exports = app;
