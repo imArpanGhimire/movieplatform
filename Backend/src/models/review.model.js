1f38be73a12f4cec9343041e9336ac99141b1fb3
imArpanGhimire <arpancalma@gmail.com>
2026-04-29 23:37:04 +0545

    fix: correct reference case for user in review schema

--- Backend/src/models/review.model.js
+++ Backend/src/models/review.model.js
@@ -8,7 +8,7 @@ const reviewschema = new mongoose.Schema({
     },
     user: {
         type: mongoose.Schema.Types.ObjectId,
-        ref: "user",
+        ref: "User",
         required: true
     },
     comment: {
