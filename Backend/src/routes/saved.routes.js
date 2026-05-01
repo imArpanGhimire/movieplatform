4cb0e67bceab77fd9e1bc03aee8d34565b4062a3
imArpanGhimire <arpancalma@gmail.com>
2026-05-01 13:16:29 +0545

    feat: add toggleLike route to manage movie likes

--- Backend/src/routes/saved.routes.js
+++ Backend/src/routes/saved.routes.js
@@ -5,6 +5,7 @@ const {
     saveMovie,
     getSavedMovies,
     removeSavedMovie,
+    toggleLike // 👈 ADD THIS
 } = require("../controllers/saved.controller");
 
 const router = express.Router();
@@ -12,5 +13,8 @@ const router = express.Router();
 router.post("/", auth, saveMovie);
 router.get("/", auth, getSavedMovies);
 router.delete("/:tmdbId", auth, removeSavedMovie);
+
+// ❤️ LIKE ROUTE
+router.post("/like", auth, toggleLike);
 
 module.exports = router;
