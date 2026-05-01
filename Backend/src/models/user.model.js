8a7cc1fe3112b46b6d1846b5c5a222d17d8f9e72
imArpanGhimire <arpancalma@gmail.com>
2026-05-01 13:16:21 +0545

    fix: format user schema for improved readability and consistency

--- Backend/src/models/user.model.js
+++ Backend/src/models/user.model.js
@@ -1,36 +1,52 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-    username: {
-        type: String,
-        required: true,
-        unique: true,
-        lowercase: true,
-    },
-
-    password: {
-        type: String,
-        required: true,
-    },
-
-    role: {
-        type: String,
-        enum: ["user", "maker"],
-        default: "user",
-    },
-
-    savedMovies: [
-        {
-            tmdbId: {
-                type: String,
-                required: true,
-            },
-            title: String,
-            poster: String,
-            releaseDate: String,
-            rating: Number,
+const userSchema = new mongoose.Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
         },
-    ],
-});
+
+        password: {
+            type: String,
+            required: true,
+        },
+
+        role: {
+            type: String,
+            enum: ["user", "maker"],
+            default: "user",
+        },
+
+        savedMovies: [
+            {
+                tmdbId: {
+                    type: String,
+                    required: true,
+                },
+                title: String,
+                poster: String,
+                releaseDate: String,
+                rating: Number,
+            },
+        ],
+
+        likedMovies: [
+            {
+                tmdbId: {
+                    type: String,
+                    required: true,
+                },
+                title: String,
+                poster: String,
+                releaseDate: String,
+                rating: Number,
+            },
+        ],
+    },
+    { timestamps: true }
+);
 
 module.exports = mongoose.model("User", userSchema);
