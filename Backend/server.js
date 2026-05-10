5281aabf4f76009af3fd950aebc38d637866b0aa
imArpanGhimire <arpancalma@gmail.com>
2026-05-11 01:54:00 +0545

    fix: update server port configuration to use environment variable

--- Backend/server.js
+++ Backend/server.js
@@ -4,7 +4,8 @@ const connectDB = require("./src/db/db")
 
 connectDB()
 
+const PORT = process.env.PORT || 3000
 
-app.listen(3000, () => {
-    console.log("server is running on port 3000")
+app.listen(PORT, () => {
+    console.log(`server is running on port ${PORT}`)
 })
