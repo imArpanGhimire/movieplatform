6a54d14b1270db812526ee3f1dc4249db6f696ff
imArpanGhimire <arpancalma@gmail.com>
2026-05-17 19:04:58 +0545

    refactor: update axios baseURL to use environment variable for API URL

--- Frontend/src/api/axios.js
+++ Frontend/src/api/axios.js
@@ -1,8 +1,7 @@
-// Frontend/src/api/axios.js
 import axios from "axios"
 
 const api = axios.create({
-    baseURL: "http://localhost:3000/api",
+    baseURL: import.meta.env.VITE_API_URL,
     withCredentials: true,
 })
 
