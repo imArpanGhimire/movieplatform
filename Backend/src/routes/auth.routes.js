3ab1a0efae33aa12c8c92b3b440b99e583f92b5a
imArpanGhimire <arpancalma@gmail.com>
2026-05-18 19:13:49 +0545

    refactor: clean up auth routes by removing unused getusers endpoint and adjusting formatting

--- Backend/src/routes/auth.routes.js
+++ Backend/src/routes/auth.routes.js
@@ -5,11 +5,13 @@ const authcontroller = require("../controllers/auth.controller");
 const authmiddlewares = require("../middlewares/auth.middleware");
 
 router.post("/register", authcontroller.registeruser);
+
 router.post("/login", authcontroller.loginuser);
+
 router.post("/logout", authcontroller.logoutuser);
 
-router.get("/getusers", authmiddlewares, authcontroller.getusers);
 router.get("/me", authmiddlewares, authcontroller.me);
+
 router.get("/profile", authmiddlewares, authcontroller.getProfile);
 
 module.exports = router;
