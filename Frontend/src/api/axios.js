557e3f39dea8bc2a445bd18487f6da4db71c5f18
imArpanGhimire <arpancalma@gmail.com>
2026-05-19 14:52:31 +0545

    fix: revert axios baseURL to use environment variable for flexibility

--- Frontend/src/api/axios.js
+++ Frontend/src/api/axios.js
@@ -1,8 +1,6 @@
 import axios from "axios"
-
 const api = axios.create({
-    baseURL: "https://filmvault-backend-bukf.onrender.com/api",
+    baseURL: import.meta.env.VITE_API_URL,
     withCredentials: true,
 })
-
 export default api
